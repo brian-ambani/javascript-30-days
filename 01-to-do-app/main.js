61ccf86484eaffc481db633bbded23d9464efdc5
brian-ambani <brainambani1@gmail.com>
2023-09-15 11:51:23 +0300

    updated the mainjs file

--- 01-to-do-app/main.js
+++ 01-to-do-app/main.js
@@ -26,4 +26,19 @@ function addTodo(event){
     todoDiv.appendChild(newTodo);
 
     //Check Mark Button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML= '<h1>Comp</h1>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML= '<h1>Del</h1>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+
+    // Append to list
+
+    todoList.appendChild(todoDiv);
 }
